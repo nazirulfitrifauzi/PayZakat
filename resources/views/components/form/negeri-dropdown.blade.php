39ab66d5403ef47ac5aa2174f8df679e1c2d07ac
Nazirul Fitri Fauzi <nazirulfitrifauzi@gmail.com>
2020-09-15 15:00:34 +0800

    Fix Dropdown selected issue

--- resources/views/components/form/negeri-dropdown.blade.php
+++ resources/views/components/form/negeri-dropdown.blade.php
@@ -3,10 +3,14 @@
         {{ $label }}
     </label>
     <div class="mt-1 rounded-md shadow-sm">
-        <select class="form-select block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5" wire:loading.attr='readonly' wire:loading.class="bg-gray-300" wire:target="submit">
-            @foreach ($value as $item)
-                <option value="{{ $item->id }}">{{ $item->description }}</option>
-            @endforeach
+        <select
+            wire:model.lazy="{{ $value }}"
+            class="form-select block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5"
+            wire:loading.attr='readonly'
+            wire:loading.class="bg-gray-300"
+            wire:target="submit"
+        >
+            {{ $slot }}
         </select>
     </div>
 </div>
