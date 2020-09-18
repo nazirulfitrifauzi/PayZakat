1c478708381d26d4cf441d608644610c2e4d9a9a
Nazirul Fitri Fauzi <nazirulfitrifauzi@gmail.com>
2020-09-18 11:01:37 +0800

    Add BM locale

--- app/View/Components/Form/SubmitCancelButton.php
+++ app/View/Components/Form/SubmitCancelButton.php
@@ -16,11 +16,6 @@ class SubmitCancelButton extends Component
         $this->submit = $submit;
     }
 
-    /**
-     * Get the view / contents that represent the component.
-     *
-     * @return \Illuminate\View\View|string
-     */
     public function render()
     {
         return view('components.form.submit-cancel-button');
