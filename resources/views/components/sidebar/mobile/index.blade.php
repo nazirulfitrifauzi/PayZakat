aae7d0ef0888d67f051911854f4155aff4e0ff09
Nazirul Fitri Fauzi <nazirulfitrifauzi@gmail.com>
2020-10-16 22:24:51 +0800

    add Byara Menu

--- resources/views/components/sidebar/mobile/index.blade.php
+++ resources/views/components/sidebar/mobile/index.blade.php
@@ -1,5 +1,5 @@
 <div x-show="sidebarOpen" class="lg:hidden" style="display: none;">
-    <div class="fixed inset-0 flex z-40">
+    <div class="fixed inset-0 z-40 flex">
         <div
             @click="sidebarOpen = false"
             x-show="sidebarOpen"
@@ -10,7 +10,7 @@
             x-transition:leave-start="opacity-100"
             x-transition:leave-end="opacity-0"
             class="fixed inset-0" style="display: none;">
-            <div class="absolute inset-0 bg-cool-gray-600 opacity-75"></div>
+            <div class="absolute inset-0 opacity-75 bg-cool-gray-600"></div>
         </div>
         <div
             x-show="sidebarOpen"
@@ -20,56 +20,60 @@
             x-transition:leave="transition ease-in-out duration-300 transform"
             x-transition:leave-start="translate-x-0"
             x-transition:leave-end="-translate-x-full"
-            class="relative flex-1 flex flex-col max-w-xs w-full pt-5 pb-4 bg-teal-600" style="display: none;">
+            class="relative flex flex-col flex-1 w-full max-w-xs pt-5 pb-4 bg-teal-600" style="display: none;">
 
-            <div class="absolute top-0 right-0 -mr-14 p-1">
-                <button x-show="sidebarOpen" @click="sidebarOpen = false" class="flex items-center justify-center h-12 w-12 rounded-full focus:outline-none focus:bg-cool-gray-600" aria-label="Close sidebar" style="display: none;">
-                    <x-heroicon-o-x class="h-6 w-6 text-white"/>
+            <div class="absolute top-0 right-0 p-1 -mr-14">
+                <button x-show="sidebarOpen" @click="sidebarOpen = false" class="flex items-center justify-center w-12 h-12 rounded-full focus:outline-none focus:bg-cool-gray-600" aria-label="Close sidebar" style="display: none;">
+                    <x-heroicon-o-x class="w-6 h-6 text-white"/>
                 </button>
             </div>
-            <div class="flex-shrink-0 flex items-center justify-center px-4">
-                <x-general.logo class="h-16 w-auto"/>
+            <div class="flex items-center justify-center flex-shrink-0 px-4">
+                <x-general.logo class="w-auto h-16"/>
             </div>
             <div class="mt-5 overflow-y-auto">
                 <nav class="px-2 space-y-1">
                     <x-sidebar.nav-item route="home" label="Halaman Utama" uri="home">
-                        <x-heroicon-o-home class="mr-4 h-6 w-6 text-teal-200 group-hover:text-teal-200 group-focus:text-teal-200 transition ease-in-out duration-150"/>
+                        <x-heroicon-o-home class="w-6 h-6 mr-4 text-teal-200 transition duration-150 ease-in-out group-hover:text-teal-200 group-focus:text-teal-200"/>
                     </x-sidebar.nav-item>
 
                     @if (auth()->user()->role == 1) <!-- agent -->
                         <x-sidebar.nav-item route="maklumatPengguna" label="Maklumat Pengguna" uri="maklumat-pengguna">
-                            <x-heroicon-o-user class="mr-4 h-6 w-6 text-teal-200 group-hover:text-teal-200 group-focus:text-teal-200 transition ease-in-out duration-150"/>
+                            <x-heroicon-o-user class="w-6 h-6 mr-4 text-teal-200 transition duration-150 ease-in-out group-hover:text-teal-200 group-focus:text-teal-200"/>
                         </x-sidebar.nav-item>
 
                         <x-sidebar.nav-item route="akaun" label="Akaun" uri="akaun">
-                            <x-heroicon-o-credit-card class="mr-4 h-6 w-6 text-teal-200 group-hover:text-teal-200 group-focus:text-teal-200 transition ease-in-out duration-150"/>
+                            <x-heroicon-o-credit-card class="w-6 h-6 mr-4 text-teal-200 transition duration-150 ease-in-out group-hover:text-teal-200 group-focus:text-teal-200"/>
                         </x-sidebar.nav-item>
 
-                    <x-sidebar.nav-item route="pembayar.senarai" label="Pembayar Zakat" uri="pembayar">
-                        <x-heroicon-o-user-group class="mr-4 h-6 w-6 text-teal-200 group-hover:text-teal-200 group-focus:text-teal-200 transition ease-in-out duration-150"/>
-                    </x-sidebar.nav-item>
+                        <x-sidebar.nav-item route="pembayar.senarai" label="Pembayar Zakat" uri="pembayar">
+                            <x-heroicon-o-user-group class="w-6 h-6 mr-4 text-teal-200 transition duration-150 ease-in-out group-hover:text-teal-200 group-focus:text-teal-200"/>
+                        </x-sidebar.nav-item>
 
-                    <x-sidebar.nav-item route="asnaf.senarai" label="Asnaf" uri="asnaf">
-                        <x-heroicon-o-users class="mr-4 h-6 w-6 text-teal-200 group-hover:text-teal-200 group-focus:text-teal-200 transition ease-in-out duration-150"/>
-                    </x-sidebar.nav-item>
+                        <x-sidebar.nav-item route="asnaf.senarai" label="Asnaf" uri="asnaf">
+                            <x-heroicon-o-users class="w-6 h-6 mr-4 text-teal-200 transition duration-150 ease-in-out group-hover:text-teal-200 group-focus:text-teal-200"/>
+                        </x-sidebar.nav-item>
 
                         <x-sidebar.nav-item route="kalkulator" label="Zakat Kalkulator" uri="kalkulator">
-                            <x-heroicon-o-calculator class="mr-4 h-6 w-6 text-teal-200 group-hover:text-teal-200 group-focus:text-teal-200 transition ease-in-out duration-150"/>
+                            <x-heroicon-o-calculator class="w-6 h-6 mr-4 text-teal-200 transition duration-150 ease-in-out group-hover:text-teal-200 group-focus:text-teal-200"/>
+                        </x-sidebar.nav-item>
+
+                        <x-sidebar.nav-item route="bayar" label="Bayar Zakat" uri="bayar">
+                            <x-heroicon-o-currency-dollar class="w-6 h-6 mr-4 text-teal-200 transition duration-150 ease-in-out group-hover:text-teal-200 group-focus:text-teal-200"/>
                         </x-sidebar.nav-item>
                     @endif
 
                     <x-sidebar.hr/>
 
                     <x-sidebar.nav-item route="home" label="Tetapan" uri="">
-                        <x-heroicon-o-cog class="mr-4 h-6 w-6 text-teal-200 group-hover:text-teal-200 group-focus:text-teal-200 transition ease-in-out duration-150"/>
+                        <x-heroicon-o-cog class="w-6 h-6 mr-4 text-teal-200 transition duration-150 ease-in-out group-hover:text-teal-200 group-focus:text-teal-200"/>
                     </x-sidebar.nav-item>
 
                     <x-sidebar.nav-item route="home" label="Bantuan" uri="">
-                        <x-heroicon-o-question-mark-circle class="mr-4 h-6 w-6 text-teal-200 group-hover:text-teal-200 group-focus:text-teal-200 transition ease-in-out duration-150"/>
+                        <x-heroicon-o-question-mark-circle class="w-6 h-6 mr-4 text-teal-200 transition duration-150 ease-in-out group-hover:text-teal-200 group-focus:text-teal-200"/>
                     </x-sidebar.nav-item>
 
                     <x-sidebar.nav-item route="home" label="Privasi" uri="">
-                        <x-heroicon-o-shield-check class="mr-4 h-6 w-6 text-teal-200 group-hover:text-teal-200 group-focus:text-teal-200 transition ease-in-out duration-150"/>
+                        <x-heroicon-o-shield-check class="w-6 h-6 mr-4 text-teal-200 transition duration-150 ease-in-out group-hover:text-teal-200 group-focus:text-teal-200"/>
                     </x-sidebar.nav-item>
                 </nav>
             </div>
