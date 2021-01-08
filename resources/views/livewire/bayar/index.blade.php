c3499ee7590ba24b21b5f5ffc5253f920ac4a515
safwan <safwan@csc.net.my>
2021-01-08 18:19:56 +0800

    center btn seterusnya

--- resources/views/livewire/bayar/index.blade.php
+++ resources/views/livewire/bayar/index.blade.php
@@ -2,7 +2,7 @@
     <x-general.card class="pt-4 bg-white rounded-lg">
         <x-general.steps :steps="$steps" :currentstep="$currentstep" />
         <div class="mt-20 sm:mt-8">
-            <div class="flex items-center justify-end gap-5 p-5 ">
+            <div class="flex items-center justify-center sm:justify-end gap-5 p-5 ">
                 <div class="flex flex-row-reverse gap-3">
                     @if ($currentstep == 1)
                         <button class="flex items-center justify-center w-40 py-3 text-sm leading-none text-white transition duration-300 ease-in-out bg-teal-400 rounded cursor-pointer hover:bg-teal-500" wire:click="navigation('next')">
